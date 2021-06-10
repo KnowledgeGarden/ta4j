d98c8b2360ebd8f577d207990bd9e9d80e6cdcfd
Jack Park <jackpark@gmail.com>
2021-06-10 15:57:41 -0700

    Started indicator

---
+++ ta4j-core/src/main/java/org/ta4j/core/indicators/aim/AIMIndicator.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.ta4j.core.indicators.aim;
+
+import org.ta4j.core.indicators.AbstractIndicator;
+import org.ta4j.core.BarSeries;
+
+/**
+ * @author jackpark
+ *
+ */
+public class AIMIndicator extends AbstractIndicator {
+	
+	public AIMIndicator(BarSeries series) {
+		super(series);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
